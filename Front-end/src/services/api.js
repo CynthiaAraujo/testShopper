556405383a7c5df25a7fbcc6dbf54890c737f5d9
Cynthia Araujo <cynthia.araujo@ee.ufcg.edu.br>
2023-05-14 16:33:32 -0300

    Api change

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://homolog.planetasec.com.br/prova/front/api/clients",
+  baseURL: "",
 });
 
 export function uploadCsv(file) {
